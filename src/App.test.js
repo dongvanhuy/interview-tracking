ec3fd5ad4a32b12a2597e9aad68589f84bfb6128
VDC\thuynh63 <quocthang.it247>
2018-08-09 13:58:43 +0700

    update UI for all devices

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
-
-it('renders without crashing', () => {
-    const app = shallow(<App />);
-    expect(app.find('main')).toBeDefined();
-});
