6ad19ce28e8cf3e4d7a99a803176a4fda9360c26
VDC\thuynh63 <quocthang.it247>
2018-08-10 14:32:14 +0700

    update in scss

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './styles/index.css';
 import Profile from './containers/profile/Profile';
 import Login from './containers/login/Login';
 import ErrorPage from './containers/common/ErrorPage';
-import ProfileDetails from './containers/profileDetails/ProfileDetails';
+// import ProfileDetails from './containers/profileDetails/ProfileDetails';
 
 class App extends Component {
     render() {
@@ -14,7 +14,7 @@ class App extends Component {
             <Switch>
                 <Route exact path="/" component={Login} />
                 <Route exact path="/profile" component={Profile} />
-                <Route exact path="/profile-detail" component={ProfileDetails} />
+                {/* <Route exact path="/profile-detail" component={ProfileDetails} /> */}
                 <Route component={ErrorPage} />
             </Switch>
             // </div>
