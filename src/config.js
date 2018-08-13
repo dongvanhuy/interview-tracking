651aec1f1b5a16d66e50cbae20b07c58b39e6e8c
VDC\hdong4 <huy.dong@hubcba.com>
2018-08-13 09:53:24 +0700

    huy refactor code

--- src/config.js
+++ src/config.js
@@ -1,14 +1,11 @@
 class Config {
     constructor() {
         this.apiService = {
-            host: 'http://interview-tracking.getsandbox.com',
+            host: 'http://dxc-interviewtracking-api.azurewebsites.net',
         };
 
         this.apiService2 = {
             host: 'https://5b62685807412d00142acf69.mockapi.io',
-        };
-        this.apiServiceProfileDetails = {
-            host: 'http://dxc-interviewtracking-api.azurewebsites.net',
         };
     }
 }
