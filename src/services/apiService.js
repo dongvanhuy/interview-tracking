9332a25f7a5092c133b8cf1c87453b63ad406f46
VDC\thuynh63 <quocthang.it247>
2018-08-13 18:15:46 +0700

    update api from dev

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,32 +1,39 @@
-import { Observable } from 'rxjs';
-import axios from 'axios';
-
-import config from '../config';
+import { Observable } from "rxjs";
+import axios from "axios";
+import config from "../config";
 
 const API_HOST = config.apiService.host;
 const API_HOST2 = config.apiService2.host;
 
-
 export default class ApiService {
-    static loadDataCandidate = () => Observable.fromPromise(axios.get(`${API_HOST}/api/interviewers`, { 
+  static loadDataCandidate = () =>
+    Observable.fromPromise(
+      axios.get(`${API_HOST}/api/interviewers`, {
         data: {},
         headers: {
-            'Content-Type': 'application/json'
+          "Content-Type": "application/json"
         }
-    }));
+      })
+    );
 }
 
 export class ApiService2 {
-    static checkUser = () => Observable.fromPromise(axios.get(`${API_HOST2}/test`, { data: {} }));
+  static checkUser = () =>
+    Observable.fromPromise(axios.get(`${API_HOST2}/test`, { data: {} }));
 }
 
 export class ApiServiceProfileDetails {
-    static loadDataProfileDetails = () => Observable.fromPromise(
-        //`${API_HOSTPROFILEDETAILS}/api/interviewers`
-        axios.get('https://5b61807107412d00142ace28.mockapi.io/apiv1/interviewers', {
-            data: {},
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }))
+  static loadDataProfileDetails = () =>
+    Observable.fromPromise(
+      //`${API_HOSTPROFILEDETAILS}/api/interviewers`
+      axios.get(
+        "https://5b61807107412d00142ace28.mockapi.io/apiv1/interviewers",
+        {
+          data: {},
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      )
+    );
 }
