55c62307fd25b76bcd92ebfc2347243c01a14bef
Internship1 <nguyenphuocvinh1997@gmail.com>
2018-08-14 16:25:37 +0700

    Get Candidate Id

--- src/containers/profile/ProfileAction.js
+++ src/containers/profile/ProfileAction.js
@@ -1,9 +1,9 @@
 import { createAction } from 'redux-actions';
-import { PROFILE_LOAD, PROFILE_LOAD_SUCCESS, VIEW_DETAIL_DATA, ADD_PROFILE} from '../../store/actionTypes';
+import { PROFILE_LOAD, PROFILE_LOAD_SUCCESS, VIEW_DETAIL_DATA, ADD_PROFILE, GET_CANDIDATE_ID} from '../../store/actionTypes';
 
 
 export const loadProfile = createAction(PROFILE_LOAD);
 export const loadProfileSuccess = createAction(PROFILE_LOAD_SUCCESS);
 export const viewDetailData = createAction(VIEW_DETAIL_DATA);
 export const addProfile = createAction(ADD_PROFILE);
-// export const getCandidateId = createAction(GET_CANDIDATE_ID);
+export const getCandidateId = createAction(GET_CANDIDATE_ID);
