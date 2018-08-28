b1e12693bda8c719c90b07e787a9101c5b3bcd5d
VDC\thuynh63 <quocthang.it247@gmail.com>
2018-08-28 11:22:39 +0700

    replace URL in adal config

--- src/adalConfig.js
+++ src/adalConfig.js
@@ -6,7 +6,7 @@ export const adalConfig = {
     endpoints: {
         api: 'fede1873-aa95-4256-b554-1aa5a89e26ef', // the same client id
     },
-    postLogoutRedirectUri: 'http://localhost:3000/',
+    postLogoutRedirectUri: 'https://dxc-interview-tracking-release.azurewebsites.net/',
     cacheLocation: 'localStorage',
 };
 
