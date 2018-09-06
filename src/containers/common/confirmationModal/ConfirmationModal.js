dc96754cf8f54a5973c7e59e00f931130483ddf9
quocthang.it247@gmail.com <>
2018-09-06 21:26:00 +0700

    test toastify

--- src/containers/common/confirmationModal/ConfirmationModal.js
+++ src/containers/common/confirmationModal/ConfirmationModal.js
@@ -5,39 +5,52 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 
 export default class ConfirmationModal extends Component {
-    defaultProps = {
-        show: false,
-        messages: '',
-        handleClose: () => {},
-        handleBackToList: () => {},
+  defaultProps = {
+      show: false,
+      messages: '',
+      handleClose: () => {},
+      handleBackToList: () => {},
+  };
 
-    }
+  propsTypes = {
+      show: PropTypes.bool,
+      messages: PropTypes.string,
+      handleClose: PropTypes.func,
+      handleBackToList: PropTypes.func,
+  };
 
-    propsTypes = {
-        show: PropTypes.bool,
-        messages: PropTypes.string,
-        handleClose: PropTypes.func,
-        handleBackToList: PropTypes.func,
-    }
+  notify = () => { toast('Wow so easy !'); console.log('>>>>>>>>>> test'); };
 
-    notify = () => toast('Wow so easy !');
-
-    render() {
-        return (
-            <Modal show={this.props.show} onHide={this.props.handleClose} className="success-modal">
-                <Modal.Header closeButton>
-                    <Modal.Title>Confirmation</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <h3>{this.props.messages}</h3>
-                    <p>{this.props.ps}</p>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button onClick={this.props.handleBackToList} className="success-modal__button-yellow">OK</Button>
-                    <Button onClick={this.props.handleClose}>CANCEL</Button>
-                </Modal.Footer>
-            </Modal>
-        );
-    }
+  render() {
+      return (
+          <React.Fragment>
+              <Modal
+                  show={this.props.show}
+                  onHide={this.props.handleClose}
+                  className="success-modal"
+              >
+                  <Modal.Header closeButton>
+                      <Modal.Title>Confirmation</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                      <h3>{this.props.messages}</h3>
+                      <p>{this.props.ps}</p>
+                  </Modal.Body>
+                  <Modal.Footer>
+                      <Button
+                          onClick={() => {
+                              this.props.handleBackToList();
+                              this.notify();
+                          }}
+                          className="success-modal__button-yellow"
+                      >
+            OK
+                      </Button>
+                      <Button onClick={this.props.handleClose}>CANCEL</Button>
+                  </Modal.Footer>
+              </Modal>
+              <ToastContainer />
+          </React.Fragment>
+      );
+  }
 }
-
