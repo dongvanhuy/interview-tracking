85a4d7f58eaa725b207a5e8282d90ac48f25688a
Trang Nguyen <trangthi.nguyen@hubcba.com>
2018-09-18 09:15:49 +0700

    new cliend id

--- src/adalConfig.js
+++ src/adalConfig.js
@@ -2,9 +2,11 @@ import { AuthenticationContext, adalFetch, withAdalLogin } from 'react-adal';
 
 export const adalConfig = {
     tenant: '93f33571-550f-43cf-b09f-cd331338d086',
-    clientId: 'fede1873-aa95-4256-b554-1aa5a89e26ef',
+    // clientId: 'fede1873-aa95-4256-b554-1aa5a89e26ef',
+    clientId: '036e0394-cb25-47bc-8102-e21d3e5292a5',
     endpoints: {
-        api: 'fede1873-aa95-4256-b554-1aa5a89e26ef', // the same client id
+        // api: 'fede1873-aa95-4256-b554-1aa5a89e26ef', // the same client id
+        api: '036e0394-cb25-47bc-8102-e21d3e5292a5',
     },
     postLogoutRedirectUri: 'https://dxc-interview-tracking-release.azurewebsites.net/',
     cacheLocation: 'localStorage',
