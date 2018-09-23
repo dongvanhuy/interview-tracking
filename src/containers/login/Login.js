67fc0b68eb0626c0c19a944f313b78bc03b757cf
VDC\hdong4 <huy.dong@hubcba.com>
2018-09-23 13:12:27 +0700

    refactor code

--- src/containers/login/Login.js
+++ src/containers/login/Login.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
 import { updateLoginInfo } from './LoginActions';
-import { authContext } from '../../adalConfig';
+import { authContext, getAccessToken } from '../../adalConfig';
 import logo from '../../../src/assets/images/dxcBlack.png';
 
 export class Login extends Component {
@@ -15,70 +15,76 @@ export class Login extends Component {
         this.loadUser();
     }
 
-  initState = {
-      email: '',
-      givenName: '',
-      surname: '',
-      loginSuccess: false,
-  };
+    initState = {
+        email: '',
+        givenName: '',
+        surname: '',
+        loginSuccess: false,
+    };
 
-  loadUser = () => {
-      const user = authContext.getCachedUser();
-      if (authContext.isCallback(window.location.hash)) {
-      // Handle redirect after token requests
-          authContext.handleWindowCallback();
-          const err = authContext.getLoginError();
-          if (err) {
-              // TODO: Handle errors signing in and getting tokens
-              // console.log('error', `${err}`);
-          }
-      } else if (user) {
-          sessionStorage.setItem('surname', user.profile.family_name);
-          sessionStorage.setItem('givenName', user.profile.given_name);
-          this.setState(
-              {
-                  email: user.profile.unique_name,
-                  givenName: user.profile.given_name,
-                  surname: user.profile.family_name,
-                  loginSuccess: true,
-              },
-              () => {
-                  this.props.updateLoginInfo(this.state);
-              },
-          );
-      } else {
-          sessionStorage.clear();
-          this.setState({ ...this.initState }, () => {
-              this.props.updateLoginInfo(this.state);
-          });
-      }
-  };
+    loadUser = () => {
+        const user = authContext.getCachedUser();
+        const token = getAccessToken;
+        console.log('token', token);
+        if (authContext.isCallback(window.location.hash)) {
+        // Handle redirect after token requests
+            authContext.handleWindowCallback();
+            const err = authContext.getLoginError();
+            if (err) {
+                // TODO: Handle errors signing in and getting tokens
+                // console.log('error', `${err}`);
+            }
+        } else if (user) {
+            sessionStorage.setItem('token', token);
+            sessionStorage.setItem('surname', user.profile.family_name);
+            sessionStorage.setItem('givenName', user.profile.given_name);
+            this.setState(
+                {
+                    email: user.profile.unique_name,
+                    givenName: user.profile.given_name,
+                    surname: user.profile.family_name,
+                    loginSuccess: true,
+                },
+                () => {
+                    this.props.updateLoginInfo(this.state);
+                },
+            );
+        } else {
+            sessionStorage.clear();
+            this.setState({ ...this.initState }, () => {
+                this.props.updateLoginInfo(this.state);
+            });
+        }
+    };
 
-  login = e => {
-      e.preventDefault();
-      authContext.login();
-  };
+    login = e => {
+        e.preventDefault();
+        authContext.login();
+    };
 
-  render() {
-      if (this.props.loginStatus) {
-          this.props.push('/profile');
-      }
-      return (
-          <section className="login">
-              <div className="login__rightSide">
-                  <div className="login__header">
-                      <img src={logo} alt="logo" />
-                      <h3>Interview Tracking</h3>
-                  </div>
-                  <div className="login__content">
-                      <button type="button" onClick={e => this.login(e)}>Sign in with global pass
-                      </button>
-                  </div>
-                  <p className="login__footer">© 2018 DXC Technology Company. All rights reserved.</p>
-              </div>
-          </section>
-      );
-  }
+    render() {
+        if (this.props.loginStatus) {
+            this.props.push('/profile');
+        }
+        return (
+            <section className="login">
+                <div className="login__rightSide">
+                    <div className="login__header">
+                        <img src={logo} alt="logo" />
+                        <h3>Interview Tracking</h3>
+                    </div>
+                    <div className="login__content">
+                        <button type="button" onClick={e => this.login(e)}>
+                Sign in with global pass
+                        </button>
+                    </div>
+                    <p className="login__footer">
+            © 2018 DXC Technology Company. All rights reserved.
+                    </p>
+                </div>
+            </section>
+        );
+    }
 }
 
 const mapStateToProps = state => ({
