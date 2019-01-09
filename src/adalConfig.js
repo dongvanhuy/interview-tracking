fe16214650795f820e89ef27bfd8fb2cf0c6582a
VDC\hdong4 <huy.dong@hubcba.com>
2019-01-09 13:23:25 +0700

    update new client id

--- src/adalConfig.js
+++ src/adalConfig.js
@@ -1,10 +1,10 @@
 import { AuthenticationContext, adalFetch, withAdalLogin } from 'react-adal';
 
 export const adalConfig = {
-    tenant: '93f33571-550f-43cf-b09f-cd331338d086',
-    clientId: 'b8ecfcc8-7fb7-4296-a4fc-67560181de98',
+    tenant: '0e96568a-2285-4e46-92e9-ea3859b3a8cc',
+    clientId: '95cdc2b1-0240-4f89-8d5d-dec221844182',
     endpoints: {
-        api: 'b8ecfcc8-7fb7-4296-a4fc-67560181de98', // the same client id
+        api: '95cdc2b1-0240-4f89-8d5d-dec221844182', // the same client id
     },
     postLogoutRedirectUri: 'https://interview-tracking.azurewebsites.net',
     cacheLocation: 'localStorage',
